fcc85bdaf489ff0e4859e473284c5a24f9b8386d
Matheus Lago Frete <mlfrete@gmail.com>
2022-09-22 16:33:43 -0300

    Update InvalidPalletData.php

--- src/Exceptions/InvalidPalletData.php
+++ src/Exceptions/InvalidPalletData.php
@@ -6,9 +6,13 @@ use Exception;
 
 class InvalidPalletData extends Exception
 {   
-    public function __construct(private $pallet_data)
+    public $pallet_id;
+    public $pallet_data;
+    
+    public function __construct($pallet_id, $pallet_data)
     {
-        
+        $this->pallet_id = $pallet_id;
+        $this->pallet_data = $pallet_data;
     }
 
     public function context()
